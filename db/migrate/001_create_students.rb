d69cf4e03343b600fa69e91707f7d6ab871dcabb
Karl Amoguis <ktamoguis@gmail.com>
2018-07-05 07:24:43 +0000

    automatically backed up by learn

--- db/migrate/001_create_students.rb
+++ db/migrate/001_create_students.rb
@@ -1,9 +1,9 @@
 class CreateStudents < ActiveRecord::Migration
   def change
     create_table :students do |t|
-      t.string :title
-      t.text :description
- 
+      t.string :first_name
+      t.string :last_name
+
       t.timestamps null: false
     end
   end
